bc858da1280ad84b42bb60e9514eeb9b3b3b3262
Oliver Forral <intrepion@gmail.com>
2023-01-08 07:16:00 -0700

    Updated App and index files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Main from "./Main";
+import { AuthProvider } from "./context/auth";
+import { Routes } from "./routes";
 
 function App() {
   return (
-    <>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-          <li>
-            <Link to="/Logout">Logout</Link>
-          </li>
-          <li>
-            <Link to="/Register">Register</Link>
-          </li>
-        </ul>
-        <hr />
-        <Main />
-      </div>
-    </>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 
